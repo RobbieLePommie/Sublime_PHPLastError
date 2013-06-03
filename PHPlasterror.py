0d10b0deae8969fdaa9d3c8ccf95351aca10ec51
Robbie <spriggs@soapcreative.com>
2013-06-03 15:28:20 +1000

    Release 0.1 - Intial Save

---
+++ PHPlasterror.py
@@ -0,0 +1,52 @@
+# Sublime 2 Package
+# Will go to file and line of last error as logged in PHP error.log
+# (C) 2013 Rob Spriggs
+#
+
+import re
+import os
+import sublime
+import sublime_plugin
+
+
+class phplasterrorCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        #sublime.status_message("Yay")
+        settings_file = 'PHPlasterror.sublime-settings'
+        settings = sublime.load_settings(settings_file)
+        if settings.has('php_error_log'):
+            log_file = settings.get('php_error_log')
+            if os.path.exists(log_file):
+                # Open the file and read the last line
+                with open(log_file, "r") as f:
+                    f.seek(0, 2)           # Seek @ EOF
+                    fsize = f.tell()        # Get Size
+                    f.seek(max(fsize-1024, 0), 0)    # Set pos @ last n chars
+                    lines = f.readlines()     # Read to end
+                    file_name = False
+                    first_line = 'Empty File'
+                    reading_line = -1
+                    while lines[reading_line] and reading_line > -10 and not file_name:
+                        line = lines[reading_line]  # Get last Line
+                        if reading_line == -1:
+                            first_line = line
+                        matchObj = re.match('\[(.*)\] (.*) in (.*) on line (.*)', line)
+                        if matchObj:
+                            error = matchObj.group(2)
+                            file_name = matchObj.group(3)
+                            line_num = matchObj.group(4)
+                        reading_line = reading_line - 1
+
+                    if file_name:
+                        if os.path.exists(file_name):
+                            self.view.window().open_file(file_name + ':' + line_num, sublime.ENCODED_POSITION)
+                            sublime.status_message(error + ' on line ' + line_num)
+                        else:
+                            sublime.status_message(error + ' on line ' + line_num + ' ** File ' + file_name + ' not found **')
+                    else:
+                        sublime.status_message('Unknown error message ' + first_line)
+            else:
+                sublime.status_message('Error log not found at ' + log_file)
+        else:
+            sublime.status_message('Log file not set')
--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 Sublime_PHPLastError
 ====================
 
-Sublime Text plugin - Jump to the file/line of the last reported PHP error ocurred
+This is a Sublime Text Plugin that allows you to jump directly to the file and line of the last reported PHP error ocurred
+
+Settings
+--------
+
+The only settings is to add the location of the PHP Error Log. You can usually find this in php.ini. If not in php.ini, errors will go to your webserver log (e.g. apache log)
+
+
+Shortcut Keys
+-------------
+
+The shortcut key ctrl+atl+d is set.
+
+
+How It Works
+------------
+
+It reads the error log and traces back for the last line with the format
+
+[Date Time] Error in FileName on line XX
+
+and will open FileName on line XX
+
+Notes
+-----
+
+You can hook this up to your code through error_log() function, for example database errors can add the correct information to the error log, and you can use this script to jump directly to the error line.
